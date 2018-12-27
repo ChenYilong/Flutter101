a498c275772d499c19a7f553ef0e0fbb1bf508a5
地风 <elonchan.cyl@alibaba-inc.com>
2018-12-28 03:51:45 +0800

    [milestone]fist flutter Demo ios  and android: add appbar,button, and image.

---
+++ flutter_first/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+// name = "Max";
+void main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget build(BuildContext context) {
+    // context.
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                      'assets/XcodeElon.jpg'),
+                  Text('Food Paradise')
+                ],
+              ),
+            ),
+          ])),
+    );
+  }
+}
